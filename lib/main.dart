7dfac461d8029ef96807b46bfaf16e1132984ae3
ashoksolanki2001 <ashoksolanki2001@gmail.com>
2023-05-04 23:50:35 +0530

    firebase integration....
    Co-Authored-By: Mayank1904 <16149915+Mayank1904@users.noreply.github.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 
 import 'src/app.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
-  runApp(const App());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // await Firebase.initializeApp();
+
+  // Pass all uncaught "fatal" errors from the framework to Crashlytics
+  // FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
+
+  runApp(App());
 }
